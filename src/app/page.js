ae018824940d83ae780a3481ce7a5fb006cd4848
Rabins <rabinsneupane07@gmail.com>
2023-02-04 00:18:25 +0530

    initialized the next.js project

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Inter } from "@next/font/google";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello</h1>
+    </main>
+  );
+}
