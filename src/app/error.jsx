73a50a08be363e22296403ac9bc62193ac9a62c6
Rabins <rabinsneupane07@gmail.com>
2023-02-05 01:20:52 +0530

    error handeling

---
+++ src/app/error.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, result }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <div className="text-center mt-10">
+      <h1>Something went wrong</h1>
+      <button className="hover:text-amber-500 mt-5" onClick={() => reset()}>
+        Try Again
+      </button>
+    </div>
+  );
+}
