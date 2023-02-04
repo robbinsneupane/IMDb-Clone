0a617cc97caa79e82df268636cdd76b3eaef625a
Rabins <rabinsneupane07@gmail.com>
2023-02-04 23:49:27 +0530

    fetch the api

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
-import { Inter } from "@next/font/google";
-import Image from "next/image";
+import Results from "@/Components/Results";
 
-const inter = Inter({ subsets: ["latin"] });
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/movie/${
+      genre === "fetchTopRated" ? "/top_rated" : "/trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 43200 } }
+  );
+
+  if (!resp.ok) {
+    throw new Error("Failed to Fetch Data");
+  }
+
+  const data = await resp.json();
+
+  const results = data.results;
+
   return (
-    <main>
-      <h1 className="text-red-400">Hello</h1>
-    </main>
+    <div>
+      <Results results={results}></Results>
+    </div>
   );
 }
